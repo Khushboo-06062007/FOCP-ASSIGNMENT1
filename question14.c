9ffaa2566d7228004cf036cab37c8720404f615b
Khushboo-06062007 <khushboo25csu112@ncuindia.edu>
2025-11-02 18:14:41 +0530

    Add prime number counting functionality
    Implement a program to count prime numbers in an array.

---
+++ question14.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1)
+        return 0;
+    for (int i = 2; i * i <= num; i++)
+        if (num % i == 0)
+            return 0;
+    return 1;
+}
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    int count = 0;
+    for (int i = 0; i < n; i++)
+        if (isPrime(arr[i]))
+            count++;
+
+    printf("Number of prime numbers in the array: %d\n", count);
+
+    return 0;
+}
