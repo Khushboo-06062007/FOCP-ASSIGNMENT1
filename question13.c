0fecfe97f786c5b2043e37e6ba95281d6300b321
Khushboo-06062007 <khushboo25csu112@ncuindia.edu>
2025-11-02 18:12:48 +0530

    Add peak element finder in question13.c
    Implement a program to find a peak element in an array.

---
+++ question13.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+    int arr[n];
+
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &arr[i]);
+
+    for (int i = 0; i < n; i++) {
+        if ((i == 0 || arr[i] >= arr[i - 1]) && (i == n - 1 || arr[i] >= arr[i + 1])) {
+            printf("Peak element found: %d\n", arr[i]);
+            return 0;
+        }
+    }
+
+    printf("No peak element found.\n");
+    return 0;
+}
